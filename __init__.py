117b9d894eb66243b5b2616e38d28663289beb91
G51DHU <gs_sidhu@hotmail.co.uk>
2022-06-10 00:59:18 +0100

    .

--- __init__.py
+++ __init__.py
@@ -7,22 +7,22 @@ class checkSettingsMeta(MycroftSkill):
         Get settings from "settingsmeta.json"
     """
     def is_led_enabled(self):
-        if self.settings.get("is_led_enabled", "") == "yes":
+        if self.settings.get("is_led_enabled") == "yes":
             return True
         return False
         
     def led_config(self):
-        self.settings.get("led_default_config", "") 
+        return self.settings.get("led_config") 
 
 
 class onMessageBusEvent(MycroftSkill):
     def initialize(self):
-        self.led_config = checkSettingsMeta.led_config
+        self.led_config = checkSettingsMeta.led_config()
         #self.add_event('mycroft.skills.initialized', update.settingsmeta_and_profile_paths())
     
         if checkSettingsMeta.is_led_enabled() == True:
             self.add_event('mycroft.ready', led.execute(self.led_config,"ready"))
-            self.add_event('recognizer_loop:wakeword', led.execute(self.led_config,"wakeword"))
+            self.add_event('recognizer_loop:wakeword', led.execute(self.led_config,"wakeword").execute())
 
                         
                     
--- modules/led.py
+++ modules/led.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 from apa102_pi.driver import apa102
 import time
 
@@ -11,26 +10,21 @@ def getInfoThenExecute(event, led_config):
 class execute():
     def __init__(self, led_dict):
         self.led_dict = led_dict
-        self.duration = led_dict[:-1]
         self.strip = apa102.APA102(num_led=12, mosi=10, sclk=11, order='rbg')
+
         if len(self.led_dict[0][0].upper()) == 2:
             self.brightness_global = True
             self.strip.set_global_brightness(self.led_dict[0][1])
             self.led_dict = self.led_dict[1:]
-        else:
+        else:   
             self.brightness_global = False
             
-    def execute_with_local_brightness(self):
-        for LED in self.led_dict:
-            self.strip.set_pixel_rgb(LED[0], f"0x{LED[1]}", LED[2])   
-        self.strip.show()
-        time.sleep(self.duration)
             
-    def execute_with_global_brightness(self):
+    def execute(self):
         for LED in self.led_dict:
             self.strip.set_pixel_rgb(LED[0], f"0x{LED[1]}")   
         self.strip.show()
-        time.sleep(self.duration) 
+        time.sleep(2) 
         
     def shutdown(self):
         self.strip.clear_strip()
